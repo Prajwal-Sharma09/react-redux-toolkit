d4203d58a6c872c8f7ee8e5ca7b209d8745126d0
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-05-27 00:13:42 +0545

    async data fetching using thunk and comparing with createAsyncThunk from redux toolkit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { add } from "../store/cartSlice";
+import { fetchProducts } from "../store/productSlice";
+import { STATUSES } from "../store/productSlice";
 
 const Products = () => {
   const dispatch = useDispatch();
+  const { data: products, status } = useSelector((state) => state.product);
 
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      console.log(data);
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(fetchProducts());
+    // const fetchProducts = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products");
+    //   const data = await res.json();
+    //   console.log(data);
+    //   setProducts(data);
+    // };
+    // fetchProducts();
+  }, [dispatch]);
 
   const handleAdd = (product) => {
     dispatch(add(product));
   };
+
+  if (status === STATUSES.LOADING) {
+    return <h2>Loading....</h2>;
+  }
+
+  if (status === STATUSES.ERROR) {
+    return <h2>Something went wrong!</h2>;
+  }
 
   return (
     <div className="productsWrapper">
